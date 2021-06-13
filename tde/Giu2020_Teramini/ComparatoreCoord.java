2e3560ddf3587d2fb760c158a6b1c5c3643e4b38
Marco <marco@Marcos-MBP.fritz.box>
2021-06-13 15:22:53 +0200

    updated Teramini

--- tde/Giu2020_Teramini/ComparatoreCoord.java
+++ tde/Giu2020_Teramini/ComparatoreCoord.java
@@ -2,6 +2,7 @@ package Giu2020_Teramini;
 
 public class ComparatoreCoord implements Comparator<Coordinata>{
     @Override
+    //quando una coordinata è più grande dell'alra?
     public int compara(Coordinata a, Coordinata b){
         if (a.getX() > b.getX()) 
     }
